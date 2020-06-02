d1e54472298f3d354d771f33888876730ff523f8
Michael Schneider <mic.sch91@gmail.com>
2020-06-02 16:53:42 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,5 +1,5 @@
 def my_hash_creator(key, value)
-  my_hash_creator = {:name, "Grace Hopper"}
+  my_hash_creator(:name, Grace "Hopper")
   return my_hash_creator
   # return a hash that includes the key and value parameters passed into this method
 end
